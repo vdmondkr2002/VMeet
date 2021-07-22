4d4baf6e6a341dd0ac3c2f6e3875579f4ed95290
vishalbdange <64411935+vishalbdange@users.noreply.github.com>
2021-07-22 18:07:20 +0530

    Joiining PAge I

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import JoiningPage from "./Components/JoiningPage/JoiningPage";
 
 function App() {
-  return <div>Video Meet Project</div>;
+
+
+  return <div>
+    <h1 style={{textAlign:"center"}}>Home</h1>
+    <br />
+    <JoiningPage />
+  </div>;
 }
 
 export default App;
