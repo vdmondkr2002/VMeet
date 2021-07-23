bec1976e2c61e11930dde1ff7f7bf5762329dc0f
Vedant Mondkar <vedantmondkar2002@gmail.com>
2021-07-23 17:53:08 +0530

    Navbar responsiveness done

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles,AppBar,Toolbar,Typography,Button,IconButton,Box,Avatar } from '@material-ui/core';
+import { makeStyles,AppBar,Toolbar,Typography,Button,IconButton,Box,Avatar,Menu,MenuItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import {GoogleLogin} from 'react-google-login'
@@ -28,6 +28,8 @@ const Navbar = () => {
   const dispatch = useDispatch()
   const history = useHistory()
   const [loggedIn,setLoggedIn] = useState(false)
+  const [mobileView,setMobileView] = useState(false)
+  const [anchorEl,setAnchorEl] = useState(null);
   const [time,setTime] = useState(Date.now())
   const user = useSelector(state => state.user)
 
@@ -38,13 +40,32 @@ const Navbar = () => {
   },[user])
 
   useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900 && window.innerWidth > 100
+        ? setMobileView(true)
+        : setMobileView(false);
+    };
+    setResponsiveness();
+    
+    window.addEventListener("resize", setResponsiveness);
+  }, []);
+
+  useEffect(() => {
     const timer = setInterval(() => {
         setTime(Date.now())
     }, 60000)
     return () => {
         clearInterval(timer)
     }
-}, [])
+  }, [])
+
+  const handleAvatarMenuClose = ()=>{
+    setAnchorEl(null)
+  }
+
+  const handleClickAvatar = (e)=>{
+    setAnchorEl(e.currentTarget)
+  }
   const googleSuccess = async (res) => {
     console.log(res)
     try {
@@ -73,26 +94,62 @@ const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
         <Typography variant="h6" className={classes.title}>
           V Meet
         </Typography>
-        <Typography variant="h6" className={classes.date}>
-          {moment(time).format(' h:mm a ⚈ MMMM Do YYYY')}
-        </Typography>
+        {
+          !mobileView && (
+            <Typography variant="h6" className={classes.date}>
+              {moment(time).format(' h:mm a ⚈ MMMM Do YYYY')}
+            </Typography>
+          )
+        }
+        
         {
           loggedIn?(
             <>
-            <IconButton>
-              <Avatar alt={`${user.name}`} src={user.profilePic} >
-              {user?.firstName?.charAt(0)}{user?.lastName?.charAt(0)}
-              </Avatar>
-            </IconButton>
-            <Button color="secondary" variant="contained" onClick={logout}>
-              Logout
-            </Button>
+            {
+              mobileView?(
+                <>
+                  <Button 
+                          aria-controls="simple-menu"
+                          aria-haspopup="true"
+                          onClick={handleClickAvatar}
+                          >
+                        <Avatar alt={`${user.name}`} src={user.profilePic} >
+                        {user?.firstName?.charAt(0)}{user?.lastName?.charAt(0)}
+                        </Avatar>
+                  </Button>
+                  <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleAvatarMenuClose}
+                        >
+                            <MenuItem onClick={handleAvatarMenuClose}>
+                              <Button color="inherit" className={classes.menuButton} onClick={logout}>
+                                  <Typography variant="h6" className={classes.menuitemText}>
+                                      Logout
+                                  </Typography>
+                              </Button>
+                            </MenuItem>
+                    </Menu>
+                </>
+              ):(
+                <>
+                  <IconButton>
+                    <Avatar alt={`${user.name}`} src={user.profilePic} >
+                    {user?.firstName?.charAt(0)}{user?.lastName?.charAt(0)}
+                    </Avatar>
+                  </IconButton>
+                  <Button color="secondary" variant="contained" onClick={logout}>
+                    Logout
+                  </Button>
+                </>
+              )
+            }
+            
             </>
           ):(
             <GoogleLogin
