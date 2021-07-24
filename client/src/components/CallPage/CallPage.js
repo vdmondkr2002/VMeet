7bbb26b178aec148aa6dcf9cb97f75b080ac2142
Vedant Mondkar <vedantmondkar2002@gmail.com>
2021-07-24 23:52:02 +0530

    Callpage before merging

--- client/src/components/CallPage/CallPage.js
+++ client/src/components/CallPage/CallPage.js
@@ -2,8 +2,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Peer from 'simple-peer'
 import io from 'socket.io-client'
-import { Container,Grid,makeStyles, Paper } from '@material-ui/core'
+import { Container,Grid,makeStyles, Paper,Avatar,Box, Typography } from '@material-ui/core'
 import CallPageFooter from './CallPageFooter/CallPageFooter'
+import clsx from 'clsx'
+import { useSelector } from 'react-redux'
 
 
 
@@ -22,12 +24,33 @@ const useStyles = makeStyles((theme)=>({
     userPaper:{
         maxWidth:"100%",
         height:"100%",
-        borderRadius:"5%"
+        borderRadius:"5%",
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center"
     },
     video:{
+        maxHeight:"100%",
         maxWidth:"100%",
-        height:"100&",
+        // height:"100&",
         borderRadius:"5%"
+    },
+    offvideo:{
+        display:"none"
+    },
+    largeAvatar: {
+        margin: "auto",
+        width: "150px",
+        height: "150px"
+    },
+    userCont:{
+        position:"relative"
+    },
+    bottomline:{
+        position:"absolute",
+        left:"5%",
+        bottom:"5%",
     }
 }))
 
@@ -37,8 +60,9 @@ const CallPage = () => {
     const classes = useStyles()
     const myVideo = useRef();
     const [stream,setStream] = useState()
-    const [videoon,setVideoOn] = useState(true)
+    const [videoon,setVideoOn] = useState(false)
     const [isAdmin,setasAdmin] = useState(true)
+    const user = useSelector(state=>state.user)
 
     const initWebRTC = async()=>{
         const currStream = await navigator.mediaDevices.getUserMedia({audio:true,video:true})
@@ -51,7 +75,8 @@ const CallPage = () => {
     
     
     useEffect(()=>{
-        initWebRTC();
+        if(videoon)
+            initWebRTC();
     },[])
 
    
@@ -61,10 +86,27 @@ const CallPage = () => {
     return (
         <Container className={classes.mainCont}>
             <Grid container className={classes.usersCont} spacing={2}>
-                <Grid item sm={6}>
+                <Grid item sm={6} className={classes.userCont}>
+                        <video playsInline className={videoon?classes.video:classes.offvideo} muted ref={myVideo} autoPlay/>
+                        {
+                            !videoon?(
+                               <Paper className={classes.userPaper}>
+                                    <Avatar src={user?.profilePic} className={clsx(classes.largeAvatar)} alt={user?.userName}>
+                                            {user?.firstName?.charAt(0)} {user?.lastName?.charAt(0)}
+                                    </Avatar>
+                                    <div className={classes.bottomline}>
+                                        <Typography variant="body">
+                                            {user.name}
+                                        </Typography>
+                                    </div>
+                                    
+                                </Paper>
+                            ):null
+                        }
+                        
                     {/* <Paper className={classes.userPaper}> */}
                         {/* User 1 */}
-                        <video playsInline className={classes.video} muted ref={myVideo} autoPlay/>
+                        
                     {/* </Paper> */}
                 </Grid>
                 <Grid item sm={6}>
@@ -83,12 +125,22 @@ const CallPage = () => {
 
                     {/* </Paper> */}
                 </Grid>
-                <Grid item sm={6}>
+                <Grid item sm={6} className={classes.userCont}>
+                        <video playsInline className={videoon?classes.video:classes.offvideo} muted ref={myVideo} autoPlay/>
+                        {
+                            !videoon?(
+                               <Paper className={classes.userPaper}>
+                                    <Avatar src={user?.profilePic} className={clsx(classes.largeAvatar)} alt={user?.userName}>
+                                            {user?.firstName?.charAt(0)} {user?.lastName?.charAt(0)}
+                                    </Avatar>
+                                </Paper>
+                            ):null
+                        }
+                        
                     {/* <Paper className={classes.userPaper}> */}
                         {/* User 4 */}
                         {/* <video playsInline muted ref={myVideo} autoPlay/> */}
-                        <video playsInline className={classes.video} muted ref={myVideo} autoPlay/>
-
+                        
                     {/* </Paper> */}
                 </Grid>
             </Grid>
